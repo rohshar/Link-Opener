dbd06c03f8760857d60090432265f75791bc9ea2
Rohan Sharan <sharanrohan@berkeley.edu>
2016-06-22 14:30:49 -0400

    extension now opens up the first 10 links in a webpage

--- content.js
+++ content.js
@@ -1,1 +1,17 @@
-alert("Initial plugin testing")
+var count = 0;
+
+chrome.runtime.onMessage.addListener(
+	function(request, sender, sendResponse) {
+		if (request.message === "clicked_browser_action") {
+			while (count < 10) {
+				var link1 = $("a[href^='http']").eq(count).attr("href");
+				console.log(link1);
+				if (typeof link1 == "undefined") {
+					break;
+				}
+				chrome.runtime.sendMessage({"message": "open_tab", "URL": link1});
+				count++;
+			}
+		}
+	}
+)
---
+++ second.js
@@ -0,0 +1,15 @@
+chrome.browserAction.onClicked.addListener(function(tab) {
+
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    var current = tabs[0];
+    chrome.tabs.sendMessage(current.id, {"message": "clicked_browser_action"});
+  });
+});
+
+chrome.runtime.onMessage.addListener(
+	function(request, sender, sendResponse) {
+		if (request.message === "open_tab") {
+			chrome.tabs.create({"url": request.URL});
+		}
+	}
+);
