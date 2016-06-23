8994fa464adcf2ede594f96d1061a786a19fed77
Rohan Sharan <sharanrohan@berkeley.edu>
2016-06-23 16:25:51 -0400

    working through problems

--- second.js
+++ second.js
@@ -1,15 +1,25 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
 
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  chrome.tabs.query({active: true}, function(tabs) {
+  	console.log("YES IT GOT HERE")
     var current = tabs[0];
     chrome.tabs.sendMessage(current.id, {"message": "clicked_browser_action"});
   });
 });
 
-chrome.runtime.onMessage.addListener(
-	function(request, sender, sendResponse) {
-		if (request.message === "open_tab") {
-			chrome.tabs.create({"url": request.URL});
+
+function test2() {
+	count = 0;
+	chrome.runtime.onMessage.addListener(
+		function(request, sender, sendResponse) {
+			if ((request.message === "open_tab") && (count < 5)) {
+				chrome.tabs.create({"url": request.URL});
+			}
+			count += 1;
 		}
-	}
-);
+	);
+}
+
+
+
+document.getElementById('buttonOpen').addEventListener('click', test2);
